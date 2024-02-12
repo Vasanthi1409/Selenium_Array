b62f2d09ad447a6020d7c0aadd84027857616ff3
Vasanthi1409 <158304622+Vasanthi1409@users.noreply.github.com>
2024-02-12 20:47:49 +0530

    Add files via upload

---
+++ Array_Value.java
@@ -0,0 +1,12 @@
+package array_Task.com;
+
+public class Array_Value {
+	
+	public static void main(String[] args) {
+
+		int[] arr = {70, 30, 40, 100, 50};
+		System.out.println(arr[1]);
+		System.out.println(arr[3]);
+	}
+
+}
